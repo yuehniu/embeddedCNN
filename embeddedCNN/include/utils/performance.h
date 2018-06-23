e8ffaa4427234ef4558e50a0544143cf175b50d4
Yue Niu <yue.niu@foxmail.com>
2018-06-23 16:00:08 +0800

    Add Code: Measure system performance.

---
+++ embeddedCNN/include/utils/performance.h
@@ -0,0 +1,25 @@
+/*
+  Desc:
+   
+    Measure the embedded system performance.
+
+  Date:
+ 
+    06/23/2018
+
+  Author:
+
+    Yue Niu
+*/
+
+class perf_counter
+{
+public:
+     uint64_t tot, cnt, calls;
+     perf_counter() : tot(0), cnt(0), calls(0) {};
+     inline void reset() { tot = cnt = calls = 0; }
+     inline void start() { cnt = sds_clock_counter(); calls++; };
+     inline void stop() { tot += (sds_clock_counter() - cnt); };
+     inline uint64_t avg_cpu_cycles() { return ((tot+(calls>>1)) / calls); };
+};
+
